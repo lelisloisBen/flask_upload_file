cbdd0287f60f2dac6b4291370ababd6d936fc803
samirbenzada <samirbenzada@gmail.com>
2020-09-23 23:37:55 -0400

    update

--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException
 from werkzeug.utils import secure_filename
-# from .helpers import *
-import helpers
+from helpers import *
 
 app = Flask(__name__)
 app.config.from_object("config")
