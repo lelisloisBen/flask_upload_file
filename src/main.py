7e18291c97244117eace38fbf99c3c6916dc4733
samirbenzada <samirbenzada@gmail.com>
2020-09-23 22:43:40 -0400

    update

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException
+from werkzeug.security import secure_filename
 
 app = Flask(__name__)
+app.config.from_object("config")
 CORS(app)
 
 # Handle/serialize errors like a JSON object
@@ -13,6 +15,43 @@ def handle_invalid_usage(error):
 @app.route('/')
 def hello_world():
     return "<div style='text-align: center; background-color: orange'><h1>Backend running...</h1><br/><h3>Welcome back samir</h3><img src='https://media.gettyimages.com/photos/woman-sitting-by-washing-machine-picture-id117852649?s=2048x2048' width='80%' /></div>"
+
+app.config.from_object("flask_s3_upload.config")
+
+from .helpers import *
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+
+	# A
+    if "user_file" not in request.files:
+        return "No user_file key in request.files"
+
+	# B
+    file = request.files["user_file"]
+
+    """
+        These attributes are also available
+
+        file.filename               # The actual name of the file
+        file.content_type
+        file.content_length
+        file.mimetype
+
+    """
+
+	# C.
+    if file.filename == "":
+        return "Please select a file"
+
+	# D.
+    if file and allowed_file(file.filename):
+        file.filename = secure_filename(file.filename)
+        output   	  = upload_file_to_s3(file, app.config["S3_BUCKET"])
+        return str(output)
+
+    else:
+        return "not upoladed, something is wrong!"
 
 
 # this only runs if `$ python src/main.py` is executed
