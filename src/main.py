4909e68d7de350bddb56855eb0fcccc92163cf11
samirbenzada <samirbenzada@gmail.com>
2020-09-25 11:50:01 -0400

    update

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from utils import APIException
 from werkzeug.utils import secure_filename
 from helpers import *
 from PIL import Image
+from io import BytesIO  
 
 app = Flask(__name__)
 app.config.from_object("config")
