dd165770c72b88cd5c2b9c5a4193bbdcbd9eb8ea
samirbenzada <samirbenzada@gmail.com>
2020-09-28 13:25:22 -0400

    update

--- src/helpers.py
+++ src/helpers.py
@@ -19,8 +19,8 @@ def upload_file_to_s3(file, bucket_name, folder_name, file_name, myType, acl="pu
     Docs = http://boto3.readthedocs.io/en/latest/guide/s3.html
     Docs S3 = https://stackabuse.com/file-management-with-aws-s3-python-and-flask/
     Docs resize img = https://auth0.com/blog/image-processing-in-python-with-pillow/#:~:text=To%20resize%20an%20image%2C%20you,Image%20with%20the%20new%20dimensions.
-    ADD buildpack in settings https://github.com/gaumire/heroku-buildpack-mysql.git
-    MYSQL Buildpack link: https://elements.heroku.com/buildpacks/gaumire/heroku-buildpack-mysql
+    Go to ressources and add ClearDB free mysql database
+    URL = mysql://b00ad00d2d8879:c6ce3d0a@us-cdbr-east-02.cleardb.com/heroku_0f90179e60a0a02?reconnect=true
     """
     # directory_name = folder_name
     # s3.put_object(Bucket=bucket_name, Key=(directory_name+'/'))
