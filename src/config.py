6cf7731c1b4703c523962deed61d79b463071c66
samirbenzada <samirbenzada@gmail.com>
2020-09-28 15:36:27 -0400

    update

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ MYSQL_HOST = os.environ.get("HOST_MYSQL")
 MYSQL_USER = os.environ.get("USER_MYSQL")
 MYSQL_PASSWORD = os.environ.get("PASS_MYSQL")
 MYSQL_DB = os.environ.get("DATABASE_MYSQL")
-
+MYSQL_CURSORCLASS = "DictCursor"
 
 SECRET_KEY = os.urandom(32)
 DEBUG = True
