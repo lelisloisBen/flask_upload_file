745da485d77e04c0d78eabee097391f708249d67
lelislois Ben <samirbenzada@gmail.com>
2020-10-19 13:26:27 -0400

    updated

--- src/config.py
+++ src/config.py
@@ -11,12 +11,6 @@ MYSQL_PASSWORD = os.environ.get("PASS_MYSQL")
 MYSQL_DB = os.environ.get("DATABASE_MYSQL")
 MYSQL_CURSORCLASS = "DictCursor"
 
-app.config['MAILGUN_KEY'] = 'YOUR-KEY-HERE'
-app.config['MAILGUN_DOMAIN'] = 'YOUR-MAILGUN-DOMAIN'
-
-
-
-
 MAILGUN_KEY = os.environ.get("MAILGUN_KEY")
 MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")
 
