9383879308dd4ea1aa65e2a8a355804fab4cd176
lelislois Ben <samirbenzada@gmail.com>
2020-10-19 00:58:14 -0400

    updated

--- src/main.py
+++ src/main.py
@@ -11,13 +11,17 @@ from flask_mail import Mail,  Message
 
 app = Flask(__name__)
 app.config.from_object("config")
-app.config.update(
-    MAIL_SERVER = 'imap.gmail.com',
-    MAIL_PORT = 993,
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = os.environ.get("EMAIL_USERNAME"),
-    MAIL_PASSWORD = os.environ.get("EMAIL_PASS")
-)
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ.get("EMAIL_USERNAME"),
+    "MAIL_PASSWORD": os.environ.get("EMAIL_PASS")
+}
+
+app.config.update(mail_settings)
 
 CORS(app)
 mysql = MySQL(app)
